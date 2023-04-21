e4968c56819ea690f5de4f3612cc41267c52bca6
Laboratorios de Informática IES 21 <laboratoriosies@hotmail.com>
2023-04-21 13:05:31 -0300

    Agregar archivos de proyecto.

---
+++ pryQuiligottiAlacena/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryQuiligottiAlacena
+{
+    public partial class frmAlacena : Form
+    {
+        public frmAlacena()
+        {
+            InitializeComponent();
+        }
+    }
+}
